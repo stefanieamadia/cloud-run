333f4631085c24bfab7274d2cb7dbde3f8042c3d
stefanieamadia <sekarhairani@gmail.com>
2024-05-10 20:12:54 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send(__dirname + '/src/index.html');
+	res.sendFile(__dirname + '/src/index.html');
 });
 
 const PORT = process.env.PORT || 8080;
