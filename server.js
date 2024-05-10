433213d2bf38007aaf842ef9c7423bd8a2d45995
stefanieamadia <sekarhairani@gmail.com>
2024-05-10 20:08:30 +0700

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get("/", (req, res) => {
-	res.send('Goodbye from Cloud Run!');
+	res.send(__dirname + '/src/index.html');
 });
 
 const PORT = process.env.PORT || 8080;
